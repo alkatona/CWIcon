872a7d69f9ef86c79242ec12dcf31f2efb39780c
alkatona <alkatona@gmail.com>
2021-05-19 22:37:06 +0200

    Add project files.

---
+++ CalendarForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace CWIcon
+{
+    partial class CalendarForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.BackColor = System.Drawing.SystemColors.Window;
+            this.monthCalendar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.monthCalendar1.Location = new System.Drawing.Point(16, 12);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.ShowTodayCircle = false;
+            this.monthCalendar1.ShowWeekNumbers = true;
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.monthCalendar1_KeyDown);
+            // 
+            // CalendarForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(230, 188);
+            this.Controls.Add(this.monthCalendar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CalendarForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "CalendarForm";
+            this.Deactivate += new System.EventHandler(this.CalendarForm_MouseCaptureChanged);
+            this.Load += new System.EventHandler(this.CalendarForm_Load);
+            this.MouseCaptureChanged += new System.EventHandler(this.CalendarForm_MouseCaptureChanged);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+    }
+}
---
+++ CalendarForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CWIcon
+{
+    public partial class CalendarForm : Form
+    {
+        public CalendarForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CalendarForm_Load(object sender, EventArgs e)
+        {
+            int x = Screen.GetWorkingArea(this).Right  - Size.Width;
+            int y = Screen.GetWorkingArea(this).Bottom - Size.Height;
+
+            this.Location = new Point(x, y);
+        }
+
+        private void CalendarForm_MouseCaptureChanged(object sender, EventArgs e)
+        {
+            this.Close();
+
+            /*
+            if (!this.Capture)
+            {
+                if (!this.RectangleToScreen(this.DisplayRectangle).Contains(Cursor.Position))
+                {
+                    this.Close();
+                }
+                else
+                {
+                    this.Capture = true;
+                }
+            }
+            */
+
+            //base.OnCaptureChanged(e);
+            // base.OnMouseCaptureChanged(e);
+        }
+
+        private void CalendarForm_Deactivate(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void monthCalendar1_KeyDown(object sender, KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if(e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void monthCalendar1_DateSelected(object sender, DateRangeEventArgs e)
+        {
+            Console.WriteLine(e.Start.ToString() + " " + e.End.ToString());
+        }
+    }
+}
