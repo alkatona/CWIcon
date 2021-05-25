2041c4eb0a189303934d4b506a2b0321aa3badd8
alkatona <alkatona@gmail.com>
2021-05-25 19:19:51 +0200

    Icon update problem solved. Added form which handles the icon. Updates on system power change and checks hourly the current week number.

--- App.cs
+++ App.cs
@@ -9,107 +9,11 @@ namespace CWIcon
 {
     public class App : ApplicationContext
     {
-        private NotifyIcon trayIcon;
-        private System.Timers.Timer timer;
-        private Int32 tick = 0;
-
+        IconCreator iconCreator;
 
         public App()
         {
-            // Initialize Tray Icon
-            trayIcon = new NotifyIcon()
-            {
-                ContextMenu = new ContextMenu(new MenuItem[] {
-                new MenuItem("Update", Update),
-                new MenuItem("Exit", Exit),
-            }),
-                Visible = true
-            };
-
-            trayIcon.Click += TrayIcon_Click;
-
-            updateTime();
+            iconCreator = new IconCreator();
         }
-
-        private void TrayIcon_Click(object sender, EventArgs e)
-        {
-            // TODO: singleton variant?
-
-            MouseEventArgs mouseEvent = (MouseEventArgs) e;
-
-            if(mouseEvent.Button == MouseButtons.Left)
-            {
-                CalendarForm calendar = new CalendarForm();
-
-                calendar.Show();
-                calendar.Activate();
-            }
-
-        }
-
-        private void setTimer()
-        {
-            timer = new System.Timers.Timer();
-            timer.Interval = 2000;
-            timer.Elapsed += OnTimerTriggerred;
-            timer.AutoReset = true;
-            timer.Enabled = true;
-        }
-
-        private int getCW()
-        {
-            Calendar myCal = CultureInfo.InvariantCulture.Calendar;
-            return myCal.GetWeekOfYear(DateTime.Now, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
-        }
-
-        private static void OnTimerTriggerred(Object source, System.Timers.ElapsedEventArgs e)
-        {
-            // something invoke solution needed here
-            // Dispatcher.Invoke(delegate { updateTime(); }, DispatcherPriority.Normal);
-        }
-
-        private void Update(object sender, EventArgs e)
-        {
-            updateTime();
-        }
-
-        private void updateTime()
-        {
-            string cw;
-
-            cw = getCW().ToString();
-
-            trayIcon.Text = "Calendar week:" + cw;
-
-            updateIconTect(cw);
-        }
-
-        private void Exit(object sender, EventArgs e)
-        {
-            trayIcon.Visible = false;
-            // timer.Stop();
-            // timer.Dispose();
-
-            System.Windows.Forms.Application.Exit();
-        }
-
-        private void updateIconTect(string inputStr)
-        {
-            Font fontToUse = new Font("Microsoft Sans Serif", 10, FontStyle.Regular, GraphicsUnit.Pixel);
-            Brush brushToUse = new SolidBrush(Color.White);
-            Bitmap bitmapText = new Bitmap((int)SystemParameters.SmallIconWidth, (int)SystemParameters.SmallIconHeight);
-            Graphics g = System.Drawing.Graphics.FromImage(bitmapText);
-
-            IntPtr hIcon;
-
-            g.Clear(Color.Transparent);
-            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
-            g.DrawString(inputStr, fontToUse, brushToUse, 0,0);
-
-            hIcon = (bitmapText.GetHicon());
-            trayIcon.Icon = System.Drawing.Icon.FromHandle(hIcon);
-
-        }
-
     }
 }
---
+++ IconCreator.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace CWIcon
+{
+    public partial class IconCreator : Form
+    {
+        private NotifyIcon trayIcon;
+        private System.Timers.Timer timer;
+
+        public IconCreator()
+        {
+            InitializeComponent();
+
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("Update", Update),
+                new MenuItem("Exit", Exit),
+            }),
+                Visible = true
+            };
+
+            trayIcon.Click += TrayIcon_Click;
+
+            updateIcon();
+
+            setupTimer();
+
+            setupSystemEventListeners();
+        }
+
+        private void setupSystemEventListeners()
+        {
+            SystemEvents.PowerModeChanged += SystemEvents_PowerModeChanged;
+        }
+
+        private void SystemEvents_PowerModeChanged(object sender, PowerModeChangedEventArgs e)
+        {
+            updateIcon();
+        }
+
+        private void setupTimer()
+        {
+            timer = new System.Timers.Timer();
+            timer.AutoReset = true;
+            timer.Interval = 1000 * 60 * 60; // check every hour
+            timer.Elapsed += Timer_Elapsed;
+            timer.Enabled = true;
+
+            timer.Start();
+        }
+
+        private int tick = 0;
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            updateIcon();
+        }
+
+        private void TrayIcon_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs mouseEvent = (MouseEventArgs)e;
+
+            if (mouseEvent.Button == MouseButtons.Left)
+            {
+                CalendarForm calendar = new CalendarForm();
+
+                calendar.Show();
+                calendar.Activate();
+            }
+
+        }
+
+        public void Exit(object sender, EventArgs e)
+        {
+            this.Close();
+            trayIcon.Visible = false;
+            timer.Enabled = false;
+
+            SystemEvents.PowerModeChanged -= SystemEvents_PowerModeChanged;
+
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void Update(object sender, EventArgs e)
+        {
+            updateIcon();
+        }
+
+        private void updateIcon()
+        {
+            string cw;
+
+            cw = getCW().ToString();
+
+            trayIcon.Text = "Calendar week:" + cw;
+
+            updateIconText(cw);
+        }
+
+        private void updateIconText(string text)
+        {
+            Font fontToUse = new Font("Microsoft Sans Serif", 10, FontStyle.Regular, GraphicsUnit.Pixel);
+            Brush brushToUse = new SolidBrush(Color.White);
+            Bitmap bitmapText = new Bitmap((int)SystemParameters.SmallIconWidth, (int)SystemParameters.SmallIconHeight);
+            Graphics g = System.Drawing.Graphics.FromImage(bitmapText);
+
+            IntPtr hIcon;
+
+            g.Clear(Color.Transparent);
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
+            g.DrawString(text, fontToUse, brushToUse, 0, 0);
+
+            hIcon = (bitmapText.GetHicon());
+            trayIcon.Icon = Icon.FromHandle(hIcon);
+        }
+
+        private int getCW()
+        {
+            Calendar myCal = CultureInfo.InvariantCulture.Calendar;
+            return myCal.GetWeekOfYear(DateTime.Now, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+        }
+    }
+}
